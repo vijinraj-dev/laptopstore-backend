abdc9c7c748cbef3643c9f64afb9f16b8650d9ad
vijinraj-dev <vijin.raj@ibosoninnov.com>
2026-04-11 21:50:46 +0530

    latest

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,33 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+
+// const pool = new Pool({
+//   host:     process.env.DB_HOST     || 'localhost',
+//   port:     parseInt(process.env.DB_PORT || '5432'),
+//   database: process.env.DB_NAME     || 'laptopstore',
+//   user:     process.env.DB_USER     || 'postgres',
+//   password: process.env.DB_PASSWORD || '0386',
+//   max: 20,
+//   idleTimeoutMillis: 30_000,
+//   connectionTimeoutMillis: 2_000,
+// });
+
+// pool.on('error', (err) => {
+//   console.error('Unexpected DB pool error', err);
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME     || 'laptopstore',
-  user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || '0386',
-  max: 20,
-  idleTimeoutMillis: 30_000,
-  connectionTimeoutMillis: 2_000,
+  connectionString: process.env. DATABASE_URL || 'postgresql://laptopstore:JuDT75go1moLFBhLQCIiT06zbJnFAKlk@dpg-d7d76lv7f7vs739msjkg-a.oregon-postgres.render.com/laptopstore',
+  ssl: {
+    rejectUnauthorized: false, // 🔥 Render க்கு important
+  },
 });
 
 pool.on('error', (err) => {
@@ -18,3 +35,4 @@ pool.on('error', (err) => {
 });
 
 module.exports = pool;
+
